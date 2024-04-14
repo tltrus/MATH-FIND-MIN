9f8e47c9a0a57a019e14267ecbc752eb184231da
tltrus <77125487+tltrus@users.noreply.github.com>
2024-04-14 20:11:23 +0400

    up

---
+++ SimulatedAnnealing (one argument)/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Chart
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea5 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series9 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series10 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnStaticStart = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnDynamicStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbTemp = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea5.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea5);
+            this.chart1.Location = new System.Drawing.Point(9, 10);
+            this.chart1.Margin = new System.Windows.Forms.Padding(2);
+            this.chart1.Name = "chart1";
+            series9.ChartArea = "ChartArea1";
+            series9.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series9.Name = "Series1";
+            series10.ChartArea = "ChartArea1";
+            series10.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series10.Name = "Series2";
+            this.chart1.Series.Add(series9);
+            this.chart1.Series.Add(series10);
+            this.chart1.Size = new System.Drawing.Size(560, 346);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnStaticStart
+            // 
+            this.btnStaticStart.Location = new System.Drawing.Point(573, 10);
+            this.btnStaticStart.Margin = new System.Windows.Forms.Padding(2);
+            this.btnStaticStart.Name = "btnStaticStart";
+            this.btnStaticStart.Size = new System.Drawing.Size(74, 31);
+            this.btnStaticStart.TabIndex = 1;
+            this.btnStaticStart.Text = "Static Start";
+            this.btnStaticStart.UseVisualStyleBackColor = true;
+            this.btnStaticStart.Click += new System.EventHandler(this.btnStaticStart_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(573, 123);
+            this.btnReset.Margin = new System.Windows.Forms.Padding(2);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(74, 30);
+            this.btnReset.TabIndex = 2;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnDynamicStart
+            // 
+            this.btnDynamicStart.Location = new System.Drawing.Point(573, 45);
+            this.btnDynamicStart.Margin = new System.Windows.Forms.Padding(2);
+            this.btnDynamicStart.Name = "btnDynamicStart";
+            this.btnDynamicStart.Size = new System.Drawing.Size(74, 47);
+            this.btnDynamicStart.TabIndex = 3;
+            this.btnDynamicStart.Text = "Dynamic start";
+            this.btnDynamicStart.UseVisualStyleBackColor = true;
+            this.btnDynamicStart.Click += new System.EventHandler(this.btnDynamicStart_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(573, 187);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Temperature:";
+            // 
+            // lbTemp
+            // 
+            this.lbTemp.AutoSize = true;
+            this.lbTemp.Location = new System.Drawing.Point(588, 211);
+            this.lbTemp.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbTemp.Name = "lbTemp";
+            this.lbTemp.Size = new System.Drawing.Size(13, 13);
+            this.lbTemp.TabIndex = 5;
+            this.lbTemp.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(656, 367);
+            this.Controls.Add(this.lbTemp);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDynamicStart);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnStaticStart);
+            this.Controls.Add(this.chart1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Find min - Simulated annealing";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnStaticStart;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnDynamicStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbTemp;
+    }
+}
+
---
+++ SimulatedAnnealing (one argument)/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Chart
+{
+    // Based on Roman Shamin solution: https://www.youtube.com/watch?v=R3XawKalzgo
+    public partial class Form1 : Form
+    {
+        Random rnd = new Random();
+        double X = 3;
+        double Y = 0;
+
+        double T = 100; // Temperature
+        double alpha = 0.98;
+        double dY = 0;
+
+        double Xnew = 0, Ynew = 0;
+        double p = 0.5;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            chart1.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            double x = 0;
+            for (x = -4; x < 4; x += 0.01)
+            {
+                double y = Func(x);
+                chart1.Series[0].Points.AddXY(x, y);
+            }
+
+            chart1.Series[1].MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Circle;
+            chart1.Series[1].Points.AddXY(X, Func(X));
+            chart1.Series[1].Points[0].Color = Color.Red;
+        }
+
+        private double Func(double x) => (x * x * (2 + Math.Cos(8 * x)));
+
+        private void Static_Annealing()
+        {
+            double alpha = 0.99;
+            double dY = 0;
+
+            double Xnew = 0, Ynew = 0;
+            double p = 0.5;
+
+            while (T > 0.005)
+            {
+                if (rnd.NextDouble() < 0.5)
+                    Xnew = X + 0.1;
+                else
+                    Xnew = X - 0.1;
+
+                Y = Func(X);
+                Ynew = Func(Xnew);
+                dY = Ynew - Y;
+
+                if (dY <= 0) // new solution better than old one
+                {
+                    X = Xnew; // particle goes to new point Xnew
+                }
+                else // new solution worse than old one (Ynew - Y) > 0
+                {
+                    p = Math.Exp(-dY / T);
+                    if (rnd.NextDouble() < p)
+                        X = Xnew;
+
+                }
+                T = alpha * T;
+            }
+
+            chart1.Series[1].MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Circle;
+            chart1.Series[1].Points.AddXY(X, Func(X));
+            chart1.Series[1].Points[1].Color = Color.Green;
+        }
+        private void Dynamic_Annealin()
+        {
+            chart1.Series[1].Points.Clear();
+            if (T > 0.001)
+            {
+                if (rnd.NextDouble() < 0.5)
+                    Xnew = X + 0.1;
+                else
+                    Xnew = X - 0.1;
+
+                Y = Func(X);
+                Ynew = Func(Xnew);
+                dY = Ynew - Y;
+
+                if (dY <= 0) // new solution better than old one
+                {
+                    X = Xnew; // particle goes to new point Xnew
+                }
+                else // new solution worse than old one (Ynew - Y) > 0
+                {
+                    p = Math.Exp(-dY / T);
+                    if (rnd.NextDouble() < p)
+                        X = Xnew;
+
+                }
+                T = alpha * T;
+            }
+
+            chart1.Series[1].MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Circle;
+            chart1.Series[1].Points.AddXY(X, Func(X));
+            chart1.Series[1].Points[0].Color = Color.Red;
+
+            lbTemp.Text = T.ToString("#.###");
+        }
+
+        private void timer1_Tick(object sender, EventArgs e) => Dynamic_Annealin();
+
+        private void btnStaticStart_Click(object sender, EventArgs e) => Static_Annealing();
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+
+            chart1.Series[1].Points.Clear();
+            X = 3;
+            Y = 0;
+            T = 100;
+            lbTemp.Text = "0";
+
+            chart1.Series[1].MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Circle;
+            chart1.Series[1].Points.AddXY(X, Func(X));
+        }
+
+        private void btnDynamicStart_Click(object sender, EventArgs e) => timer1.Start();
+    }
+}
